0961a4d1686f9ac1bb9da7822396a29279241d4c
Ivan <ivanvlora@gmail.com>
2018-05-11 09:06:20 +0900

    Budget Controller
    - created the addItem function to create new items into the budget

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // BUDGET CONTROLLER
 var budgetController = (function () {
 
-    var Expense = function(id, description, value) {
+    var Expense = function (id, description, value) {
         this.id = id;
         this.description = description;
         this.value = value;
@@ -12,6 +12,39 @@ var budgetController = (function () {
         this.description = description;
         this.value = value;
     }
+
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    }
+
+    return {
+        addItem: function (type, des, val) {
+            var newItem, ID;
+
+            // Create new ID
+            ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+
+            // Create new item based on 'inc' or 'exp' type
+            if (type === 'exp') {
+                newItem = new Expense(ID, des, val);
+            } else if (type === 'inc') {
+                newItem = new Income(ID, des, val);
+            }
+
+            // Push it into our data structure
+            data.allItems[type].push(newItem);
+
+            // Return the new element
+            return newItem;
+        }
+    };
 
 })();
 
@@ -52,7 +85,7 @@ var controller = (function (budgetCtrl, UICtrl) {
             }
         });
     };
-    
+
     var ctrlAddItem = function () {
 
         // 1. Get the field input data
@@ -69,7 +102,7 @@ var controller = (function (budgetCtrl, UICtrl) {
     };
 
     return {
-        init: function() {
+        init: function () {
             setupEventListeners();
         }
     };
